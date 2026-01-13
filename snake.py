de82076f8ae7a0032a0b0802026d0fb49a6270bd
YunaH <Yunahan53@gmail.com>
2026-01-12 21:47:39 -0500

    Created Snake class to create the initial snake and added basic movements and directions methods.

---
+++ snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+STARTING_POSITION = {(0, 0), (-20, 0), (-40, 0)}
+STEP = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.snake_segments = []
+        self.create_snake()
+        self.head = self.snake_segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_snake_seg = Turtle("square")
+            new_snake_seg.goto(position)
+            new_snake_seg.color("white")
+            new_snake_seg.penup()
+            self.snake_segments.append(new_snake_seg)
+
+    def move(self):
+        for seg_num in range(len(self.snake_segments) - 1, 0, -1):
+            x = self.snake_segments[seg_num - 1].xcor()
+            y = self.snake_segments[seg_num - 1].ycor()
+            self.snake_segments[seg_num].goto(x, y)
+        self.snake_segments[0].forward(STEP)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
