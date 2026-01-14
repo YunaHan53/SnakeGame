c6947bce455cd07017b4c9650e5a28acaad28d41
YunaH <Yunahan53@gmail.com>
2026-01-14 00:42:53 -0500

    Added a scoreboard to the screen and logic to detect when the snake 'collides' or 'eats' the food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 # Screen setup
@@ -10,9 +11,9 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 # Snake movement
 screen.listen()
@@ -26,5 +27,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.move()
+        score.scoring()
 
 screen.exitonclick()
